86a608be0a49cd30e7512b94f273da12da379ac8
asaduzzamanhimel26 <41018961+asaduzzamanhimel26@users.noreply.github.com>
2018-11-04 23:39:36 +0600

    totally simple number show project
    Here,first of all insert the the number randomly,the show the number in combobox is sorted.

---
+++ NumberStorage/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace NumberStorage
+{
+    partial class NumberStorageUI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.insertButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.showButton = new System.Windows.Forms.Button();
+            this.numberTextBox = new System.Windows.Forms.TextBox();
+            this.numberListBox = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.fromTextBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.toTextBox = new System.Windows.Forms.TextBox();
+            this.showNumberComboBox = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // insertButton
+            // 
+            this.insertButton.Location = new System.Drawing.Point(323, 16);
+            this.insertButton.Name = "insertButton";
+            this.insertButton.Size = new System.Drawing.Size(75, 23);
+            this.insertButton.TabIndex = 0;
+            this.insertButton.Text = "INSERT";
+            this.insertButton.UseVisualStyleBackColor = true;
+            this.insertButton.Click += new System.EventHandler(this.insertButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(41, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Number";
+            // 
+            // showButton
+            // 
+            this.showButton.Location = new System.Drawing.Point(275, 278);
+            this.showButton.Name = "showButton";
+            this.showButton.Size = new System.Drawing.Size(75, 23);
+            this.showButton.TabIndex = 2;
+            this.showButton.Text = "SHOW";
+            this.showButton.UseVisualStyleBackColor = true;
+            this.showButton.Click += new System.EventHandler(this.showButton_Click);
+            // 
+            // numberTextBox
+            // 
+            this.numberTextBox.Location = new System.Drawing.Point(121, 19);
+            this.numberTextBox.Name = "numberTextBox";
+            this.numberTextBox.Size = new System.Drawing.Size(154, 20);
+            this.numberTextBox.TabIndex = 3;
+            // 
+            // numberListBox
+            // 
+            this.numberListBox.FormattingEnabled = true;
+            this.numberListBox.Location = new System.Drawing.Point(121, 73);
+            this.numberListBox.Name = "numberListBox";
+            this.numberListBox.Size = new System.Drawing.Size(154, 121);
+            this.numberListBox.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(41, 220);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "FROM";
+            // 
+            // fromTextBox
+            // 
+            this.fromTextBox.Location = new System.Drawing.Point(95, 217);
+            this.fromTextBox.Name = "fromTextBox";
+            this.fromTextBox.Size = new System.Drawing.Size(79, 20);
+            this.fromTextBox.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(198, 220);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(22, 13);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "TO";
+            // 
+            // toTextBox
+            // 
+            this.toTextBox.Location = new System.Drawing.Point(249, 217);
+            this.toTextBox.Name = "toTextBox";
+            this.toTextBox.Size = new System.Drawing.Size(79, 20);
+            this.toTextBox.TabIndex = 3;
+            // 
+            // showNumberComboBox
+            // 
+            this.showNumberComboBox.FormattingEnabled = true;
+            this.showNumberComboBox.Location = new System.Drawing.Point(64, 278);
+            this.showNumberComboBox.Name = "showNumberComboBox";
+            this.showNumberComboBox.Size = new System.Drawing.Size(121, 21);
+            this.showNumberComboBox.TabIndex = 5;
+            // 
+            // NumberStorageUI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(410, 326);
+            this.Controls.Add(this.showNumberComboBox);
+            this.Controls.Add(this.numberListBox);
+            this.Controls.Add(this.toTextBox);
+            this.Controls.Add(this.fromTextBox);
+            this.Controls.Add(this.numberTextBox);
+            this.Controls.Add(this.showButton);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.insertButton);
+            this.Name = "NumberStorageUI";
+            this.Text = "Number Storage UI";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button insertButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button showButton;
+        private System.Windows.Forms.TextBox numberTextBox;
+        private System.Windows.Forms.ListBox numberListBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox fromTextBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox toTextBox;
+        private System.Windows.Forms.ComboBox showNumberComboBox;
+    }
+}
+
---
+++ NumberStorage/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NumberStorage
+{
+    public partial class NumberStorageUI : Form
+    {
+        public NumberStorageUI()
+        {
+            InitializeComponent();
+        }
+        List<int> numbers = new List<int>();
+        private void insertButton_Click(object sender, EventArgs e)
+        {
+            if(numberTextBox.Text!="")
+                {
+                numberListBox.DataSource = null;
+                int number = Convert.ToInt32(numberTextBox.Text);
+                numberTextBox.Text = "";
+                numbers.Add(number);
+                numbers.Sort();
+                numberListBox.DataSource = numbers;
+            }
+            //MessageBox.Show("Please insert the number in the correct fill");
+        }
+
+        private void showButton_Click(object sender, EventArgs e)
+        {
+            showNumberComboBox.Items.Clear();
+            int fromNumber = Convert.ToInt32(fromTextBox.Text);
+            int toNumber = Convert.ToInt32(toTextBox.Text);
+            foreach (var number in numbers)
+            {
+                if(number>=fromNumber && number<=toNumber)
+                {
+                    showNumberComboBox.Items.Add(number);
+                }
+            }
+        }
+    }
+}
